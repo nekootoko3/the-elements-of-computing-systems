104915bb9213e67416d7ea553ded7d23fe6b113a
nekootoko3 <miyako.kerria@gmail.com>
2020-03-16 23:38:02 +0900

    Finished projects 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,45 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    @SCREEN
+    D=A
+    @address     // reset draw target address
+    M=D
+    @KBD       // check if any keyboard is pressed or not
+    D=M
+    @UNPRESSED
+    D;JEQ
+    @PRESSED
+    0;JMP
+(PRESSED)
+    @color
+    M=-1
+    @DRAW
+    0;JMP
+(UNPRESSED)
+    @color
+    M=0
+    @DRAW
+    0;JMP
+(DRAW)
+    @color     // set color
+    D=M
+    @address
+    A=M        // set draw target register address to A register
+    M=D        // *address <- color
+    D=A+1
+    @address
+    M=D
+    @24575
+    D=D-A
+    @LOOP
+    D;JGT      // JMP if draw target address reaches end point
+    @DRAW
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @i
+    M=1   // i = 1
+    @R2
+    M=0   // R2 = 0
+(LOOP)
+    @i
+    D=M   // D = i
+    @R1
+    D=D-M // D = i - R1
+    @END
+    D;JGT
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
